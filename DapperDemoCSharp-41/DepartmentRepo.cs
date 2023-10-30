b308de8e862646d74510e59e119323b27b50a89b
SethBowman <brandonsethbowman@gmail.com>
2023-10-30 10:26:53 -0500

    demo completed

---
+++ DapperDemoCSharp-41/DepartmentRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DapperDemoCSharp_41
+{
+    public class DepartmentRepo : IDepartmentRepo
+    {
+        private readonly IDbConnection _connection;
+
+        public DepartmentRepo(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM departments;");
+        }
+
+        public void InsertDepartment(string newName)
+        {
+            _connection.Execute("INSERT INTO departments (Name) VALUES (@newName);",
+                new { newName });
+        }
+    }
+}
